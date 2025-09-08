5a1808646c27d735a83b8e197fbbb1691375680c
Lucas Marco <lucas.paulamarco@hotmail.com.br>
2025-09-08 11:07:07 -0300

    AULA array

---
+++ Aulas de array/aula1.js
@@ -0,0 +1,13 @@
+//               0          1        2 
+const nomes = ['Lucas',  'renata', 'Luan'];
+//string, objetos, funçoes, numeros
+// funcao construtora de array const nomes = new array ('Eduardo', 'Lucas', 'Renata')
+
+/*
+nomes[2] = 'joao';
+delete nomes [2];
+console.log(nomes);
+*/
+
+const  nome = nomes.join(' ' )
+console.log(nome)
