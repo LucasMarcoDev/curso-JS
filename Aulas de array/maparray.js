74838712d5576e855f85836932fd6582e0aabdda
Lucas Marco <lucas.paulamarco@hotmail.com.br>
2025-09-10 16:57:02 -0300

    aula array metodo map

---
+++ Aulas de array/maparray.js
@@ -0,0 +1,6 @@
+//map e uma funçao extremamente parecida com filter o que muda que o retorno final vc pode mudar os valores de array filter nao faz isso!, ele nao muda o valor original do array e sim o do array usada em map e map sempre retornara um array do mesmo tamanho do array original
+
+ const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+ const numerosEmDobro = numeros.map(function(valor){
+    console.log();
+ });
